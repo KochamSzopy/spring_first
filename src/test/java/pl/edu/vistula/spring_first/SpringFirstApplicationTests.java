e2f5a9c25cb02767fc7aea171ff4f6614dc2382f
KochamSzopy <mac.wierzbicki@gmail.com>
2025-01-23 17:18:37 +0100

    First commit

---
+++ src/test/java/pl/edu/vistula/spring_first/SpringFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.spring_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
